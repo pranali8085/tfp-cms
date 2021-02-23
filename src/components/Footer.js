c7046243bbf3ec9ec9645c8e5c867df654af0367
pranali <pranalin@imaginovation.net>
2021-02-23 22:24:08 +0530

    Footer WIP

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -107,6 +107,48 @@ const Footer = class extends React.Component {
             </div>
           </div>
         </div>
+        <section className="footer-base-strip">
+          <div className="tnc-section">
+            <p>Tales from the PROS is a registered podcast network of Imaginovation, LLC © 2018 |
+            <Link className="navbar-item" to="/contact">
+              Privacy
+            </Link>
+            </p>
+          </div>
+          <div className="social-links">
+          <div className="column is-4 social">
+                <a title="facebook" href="https://facebook.com">
+                  <img
+                    src={facebook}
+                    alt="Facebook"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="twitter" href="https://twitter.com">
+                  <img
+                    className="fas fa-lg"
+                    src={twitter}
+                    alt="Twitter"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="instagram" href="https://instagram.com">
+                  <img
+                    src={instagram}
+                    alt="Instagram"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="vimeo" href="https://vimeo.com">
+                  <img
+                    src={vimeo}
+                    alt="Vimeo"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+              </div>
+          </div>
+        </section>
       </footer>
     )
   }
